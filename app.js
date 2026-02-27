526c2b9f463483324f28283606dd0e99b2507406
Anshuman Sohan Kharbade <anshumankharbade28@gmail.com>
2026-02-27 21:34:59 +0530

    Refactor game logic and enhance UI design for improved user experience

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let msg = document.querySelector("#msg");
 
 let turnO = true;
 let count = 0;
+
 const winPat = [
   [0, 1, 2],
   [0, 3, 6],
@@ -14,7 +15,7 @@ const winPat = [
   [2, 5, 8],
   [2, 4, 6],
   [3, 4, 5],
-  [6, 7, 8],
+  [6, 7, 8]
 ];
 
 const resetGame = () => {
@@ -22,6 +23,45 @@ const resetGame = () => {
   count = 0;
   enableBoxes();
   msgContainer.classList.add("hide");
+};
+
+const disableBoxes = () => {
+  boxes.forEach(box => box.disabled = true);
+};
+
+const enableBoxes = () => {
+  boxes.forEach(box => {
+    box.disabled = false;
+    box.innerText = "";
+  });
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `🏆 ${winner} wins! 🏆`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const gameDraw = () => {
+  msg.innerText = "It's a draw!";
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPat) {
+    let pos1 = boxes[pattern[0]].innerText;
+    let pos2 = boxes[pattern[1]].innerText;
+    let pos3 = boxes[pattern[2]].innerText;
+
+    if (pos1 !== "" && pos2 !== "" && pos3 !== "") {
+      if (pos1 === pos2 && pos2 === pos3) {
+        showWinner(pos1);
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 boxes.forEach((box) => {
@@ -36,51 +76,13 @@ boxes.forEach((box) => {
     box.disabled = true;
     count++;
 
-    let isWinner = checkWinner();
+    let winnerFound = checkWinner();
 
-    if (count === 9 && !isWinner) {
-        msg.innerText = "It's a tie!"
-        gameDraw();
+    if (count === 9 && !winnerFound) {
+      gameDraw();
     }
   });
 });
 
-const gameDraw = () => {
-  msgContainer.classList.remove("hide");
-  disableBoxes();
-};
-
-const disableBoxes = () => {
-  for (let box of boxes) {
-    box.disabled = true;
-  }
-};
-
-const enableBoxes = () => {
-  for (let box of boxes) {
-    box.disabled = false;
-    box.innerText = "";
-  }
-};
-
-const showWinner = (winner) => {
-  msg.innerText = `Congratulations, Winner is ${winner}`;
-  msgContainer.classList.remove("hide");
-  disableBoxes();
-};
-
-const checkWinner = () => {
-  for (let pattern of winPat) {
-    let pos1Val = boxes[pattern[0]].innerText;
-    let pos2Val = boxes[pattern[1]].innerText;
-    let pos3Val = boxes[pattern[2]].innerText;
-
-    if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
-      if (pos1Val === pos2Val && pos2Val === pos3Val) {
-        showWinner(pos1Val);
-      }
-    }
-  }
-};
 newGameBtn.addEventListener("click", resetGame);
 resetBtn.addEventListener("click", resetGame);
